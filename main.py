be4753cb852bad423f501b9334a2567951914236
coppoladayo <gkjws767@gmail.com>
2026-07-05 05:19:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -71,8 +71,8 @@ def main():
 
         feed_title = feed.feed.get("title", "X公式")
         post_title = latest.get("title", "新しい投稿")
-        link = latest.get("link", feed_url)
-        entry_id = latest.get("id") or link
+　      link = latest.get("link", feed_url)
+        entry_id = link
 
         if sent.get(feed_url) == entry_id:
             print("新着なし:", feed_title)
